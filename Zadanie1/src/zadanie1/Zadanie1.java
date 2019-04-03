ff09c0b8cf9f61caa854e61e71e8940db598db0e
Magdalena Czechura <czechura4@o2.pl>
2019-04-03 14:14:55 +0200

    hello message

--- Zadanie1/src/zadanie1/Zadanie1.java
+++ Zadanie1/src/zadanie1/Zadanie1.java
@@ -16,6 +16,8 @@ public class Zadanie1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello friends! ");
+        
     }
     
 }
