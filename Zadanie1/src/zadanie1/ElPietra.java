e7295fd13e9e788238de861e5a8f6a344cbd69a7
Michal <Michal@hotmail.pl>
2019-04-17 14:18:23 +0200

    nowy commit

---
+++ Zadanie1/src/zadanie1/ElPietra.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zadanie1;
+
+/**
+ *
+ * @author student
+ */
+public abstract class ElPietra {
+    public int numerPietra;
+    
+}
